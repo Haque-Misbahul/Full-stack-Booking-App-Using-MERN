ed1f9775e1d0a1dd2cfecaef0bc9d7c99df2b26b
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-09-09 00:33:02 +0200

    Login complete

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -6,9 +6,12 @@ export const UserContext = createContext({});
 export function UserContextProvider({children}) {
 
     const [user, setUser] = useState(null);
-    useEffect(() => {
+    useEffect( () => {
         if (!user) {
-            axios.get('/profile')
+            axios.get('/profile').then(({data}) => {
+                setUser(data);
+            });
+            
         }
     }, []);
 
