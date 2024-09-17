20c6fe643767e8cb4845f056e863ddc1b17665db
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-09-17 20:20:09 +0200

    Complete the functionality for adding photo from link

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -75,13 +75,13 @@ export default function PlacesPage() {
                              type="text" placeholder="Add using a link....jpg" />
                             <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
                         </div>
-                        <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 ">
+                        <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6 ">
                             {addPhotoByLink.length>0 && addedPhotos.map(link =>(
-                                <div className="rounded-2xl">
-                                    <img src={'http://localhost:4000/uploads/'+link} alt="" />
+                                <div>
+                                    <img className="rounded-2xl" src={'http://localhost:4000/uploads/'+link} alt="" />
                                 </div>
                             ))}
-                            <button className="border flex gap-1 justify-center bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+                            <button className="border flex items-center gap-1 justify-center bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                                 </svg>
