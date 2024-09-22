2d22e746bbff2bc77ace21d21590d61c2dcd19d2
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-09-23 00:55:13 +0200

    Working on list of all places

--- client/src/Pages/PlacesPage.jsx
+++ client/src/Pages/PlacesPage.jsx
@@ -19,6 +19,8 @@ export default function PlacesPage() {
             <AccountNav />
             {action !== 'new' && (
                 <div className="text-center">
+                    List of all added places
+                    <br />
                     <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full" to={'/account/places/new'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
